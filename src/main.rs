1c4d354175c746fe15e2adb7fb6873826d93866c
Jeudi Prando Araujo <jeudi.araujo@el.com.br>
2020-07-10 17:22:30 -0300

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use dotenv;
 use tokio_postgres::{Error, NoTls};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     
-    let pg_host = std::env::var("PGHOST").unwrap_or_else(|_| "127.0.0.1".to_owned());
-    let pg_port = std::env::var("PGPORT").unwrap_or_else(|_| "5432".to_owned());
-    let pg_user = std::env::var("PGUSER").unwrap_or_else(|_| "postgres".to_owned());
-    let pg_pass = std::env::var("PGPASS").unwrap_or_else(|_| "".to_owned());
-    let pg_data = std::env::var("PGDATA").unwrap_or_else(|_| "postgres".to_owned());
+    let pg_host = dotenv::var("PGHOST").unwrap_or_else(|_| "127.0.0.1".to_owned());
+    let pg_port = dotenv::var("PGPORT").unwrap_or_else(|_| "5432".to_owned());
+    let pg_user = dotenv::var("PGUSER").unwrap_or_else(|_| "postgres".to_owned());
+    let pg_pass = dotenv::var("PGPASS").unwrap_or_else(|_| "".to_owned());
+    let pg_data = dotenv::var("PGDATA").unwrap_or_else(|_| "postgres".to_owned());
     
     let conn_string = format!(
         "host={} port={} user={} password={} dbname={} application_name={}",
